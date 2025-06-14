1e6bcb01b4391b56ae5e1ea80fab2b0f8a7ec6c7
akvarij <aljaz.resnik@gmail.com>
2025-06-14 18:55:51 +0200

    add plavanje

--- frontend/src/components/SportCategory/SportCategory.tsx
+++ frontend/src/components/SportCategory/SportCategory.tsx
@@ -3,6 +3,8 @@ import {
   PiPersonSimpleRunLight,
 } from "react-icons/pi";
 
+import { TbSwimming } from "react-icons/tb";
+
 import TrailRunning from "../../../public/TrailRunning.png";
 import Triatlon from "../../../public/Triatlon.png";
 
@@ -10,22 +12,14 @@ export default function SportCategory({ category }: { category: string }) {
   switch (category) {
     case "kolo":
       return <PiPersonSimpleBikeLight size={30} />;
+    case "plavanje":
+      return <TbSwimming size={30} />;
     case "trail":
-      return (
-        <img
-          src={TrailRunning}
-          alt="Trail Running"
-        />
-      );
+      return <img src={TrailRunning} alt="Trail Running" />;
     case "tek":
       return <PiPersonSimpleRunLight size={30} />;
     case "triatlon":
-      return (
-        <img
-          src={Triatlon}
-          alt="Triatlon"
-        />
-      );
+      return <img src={Triatlon} alt="Triatlon" />;
     default:
       return null;
   }
